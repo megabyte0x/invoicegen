49a01adf5d908aeabcfd0288f8fe2868bd004f00
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2026-06-05 13:34:38 +0530

    Add app icon to macOS bundles

--- script/package_release.sh
+++ script/package_release.sh
@@ -18,9 +18,36 @@ APP_MACOS="$APP_CONTENTS/MacOS"
 APP_RESOURCES="$APP_CONTENTS/Resources"
 APP_BINARY="$APP_MACOS/$APP_NAME"
 INFO_PLIST="$APP_CONTENTS/Info.plist"
+APP_ICON_NAME="InvoiceGenAppIcon"
+APP_ICON_SOURCE="$ROOT_DIR/Sources/InvoiceGenApp/Resources/invoicegen-logo.png"
 DMG_PATH="$RELEASE_DIR/$APP_NAME-$VERSION.dmg"
 NOTARY_SUBMISSION_JSON="$RELEASE_DIR/$APP_NAME-$VERSION-notary-submission.json"
 NOTARY_RESULT_JSON="$RELEASE_DIR/$APP_NAME-$VERSION-notary-result.json"
+
+install_app_icon() {
+  if [[ ! -f "$APP_ICON_SOURCE" ]]; then
+    echo "missing app icon source: $APP_ICON_SOURCE" >&2
+    exit 1
+  fi
+
+  local iconset_dir="$APP_RESOURCES/$APP_ICON_NAME.iconset"
+  rm -rf "$iconset_dir"
+  mkdir -p "$iconset_dir"
+
+  sips -z 16 16 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_16x16.png" >/dev/null
+  sips -z 32 32 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_16x16@2x.png" >/dev/null
+  sips -z 32 32 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_32x32.png" >/dev/null
+  sips -z 64 64 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_32x32@2x.png" >/dev/null
+  sips -z 128 128 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_128x128.png" >/dev/null
+  sips -z 256 256 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_128x128@2x.png" >/dev/null
+  sips -z 256 256 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_256x256.png" >/dev/null
+  sips -z 512 512 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_256x256@2x.png" >/dev/null
+  sips -z 512 512 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_512x512.png" >/dev/null
+  sips -z 1024 1024 "$APP_ICON_SOURCE" --out "$iconset_dir/icon_512x512@2x.png" >/dev/null
+
+  iconutil -c icns "$iconset_dir" -o "$APP_RESOURCES/$APP_ICON_NAME.icns"
+  rm -rf "$iconset_dir"
+}
 
 swift build -c release --product "$APP_NAME"
 BUILD_BINARY="$(swift build -c release --show-bin-path)/$APP_NAME"
@@ -44,6 +71,8 @@ cat >"$INFO_PLIST" <<PLIST
   <string>$APP_NAME</string>
   <key>CFBundleDisplayName</key>
   <string>$APP_NAME</string>
+  <key>CFBundleIconFile</key>
+  <string>$APP_ICON_NAME</string>
   <key>CFBundlePackageType</key>
   <string>APPL</string>
   <key>CFBundleShortVersionString</key>
@@ -62,6 +91,7 @@ cat >"$INFO_PLIST" <<PLIST
 </plist>
 PLIST
 
+install_app_icon
 if [[ "$CODESIGN_IDENTITY" == "-" ]]; then
   codesign --force --deep --sign "$CODESIGN_IDENTITY" "$APP_BUNDLE"
 else
